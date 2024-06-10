3a60b85f71c1076ad2e47e3a7fb85cc07e11c4cc
Ivan <ilyin1997@icloud.com>
2024-06-10 23:29:19 +0300

    fix bag

--- src/main/java/ru/salfa/messenger/service/ChatService.java
+++ src/main/java/ru/salfa/messenger/service/ChatService.java
@@ -8,7 +8,6 @@ import ru.salfa.messenger.dto.model.AttachmentsDto;
 import ru.salfa.messenger.dto.model.ChatIsCreated;
 import ru.salfa.messenger.dto.model.ChatsDto;
 import ru.salfa.messenger.dto.model.MessageDto;
-import ru.salfa.messenger.entity.Messages;
 import ru.salfa.messenger.entity.User;
 import ru.salfa.messenger.message.MessageToUser;
 
@@ -17,11 +16,17 @@ import java.util.List;
 public interface ChatService {
     @Transactional(readOnly = true)
     List<ChatsDto> getListChatDtoByUser(User user) throws JsonProcessingException;
+
     @Transactional(readOnly = true)
     List<MessageDto> searchMessage(Long chatId, String query, Long userId);
+
     void clearChat(Long chatId, Long userId);
+
     MessageDto createAndSaveMsg(Long chatId, Long senderId, String message, List<AttachmentsDto> attachments) throws JsonProcessingException;
+
     void deleteMessage(Long messageId, Long userId, boolean isAll);
+
     void sendMessage(WebSocketSession session, MessageToUser message);
+
     ChatIsCreated getChat(Long participantId, Long userId);
 }
