b9614e1c94d0827e85145cec300fc9bd1ba44e31
Max Khrapatiy <max.khrapatiy@gmail.com>
2024-07-25 14:12:03 +0300

    Add user for test (step 2).

--- src/main/resources/db/migration/V2__add_test_users.sql
+++ src/main/resources/db/migration/V2__add_test_users.sql
@@ -5,3 +5,10 @@ VALUES
 
 INSERT INTO public.otp_codes(otp_code, user_id, created, expires, is_expired)
 SELECT '00000', id, now(), '9999-12-31', false From public.users WHERE phone IN ('9000000001', '9000000002');
+
+CREATE OR REPLACE VIEW public.user_and_otp_code AS
+SELECT t1.id, t1.phone, t2.otp_code
+FROM users t1 INNER JOIN otp_codes t2 ON t2.user_id = t1.id
+WHERE
+    (now() >= t2.created AND now() <= t2.expires AND t2.is_expired = false)
+   OR t1.phone IN ('9000000001', '9000000002');
